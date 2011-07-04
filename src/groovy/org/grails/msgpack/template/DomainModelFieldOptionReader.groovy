1ee1f351628af43fef7c4a42bf63576a91cf8e5c
ohneda <ohneda@gmail.com>
2011-07-03 23:29:41 -0700

    determine a field option in grails domain model based on constraints properties

--- src/groovy/org/grails/msgpack/template/DomainModelFieldOptionReader.groovy
+++ src/groovy/org/grails/msgpack/template/DomainModelFieldOptionReader.groovy
@@ -1,5 +1,7 @@
 package org.grails.msgpack.template
 
+import java.lang.reflect.Field;
+
 import org.msgpack.template.BeansFieldEntry;
 import org.msgpack.template.FieldOption;
 import org.msgpack.template.FieldOptionReader;
@@ -10,8 +12,16 @@ public class DomainModelFieldOptionReader implements FieldOptionReader {
     @Override
     public FieldOption read( Class<?> targetClass, BeansFieldEntry entry,
             FieldOption implicitOption ) {
-        // TODO Auto-generated method stub
-        return null;
+
+            if(['id', 'version'].contains(entry.name)){
+                return FieldOption.OPTIONAL
+            }
+
+            if(msgpack.Message.constraints."${entry.name}"?.nullable){
+                return FieldOption.OPTIONAL
+            }
+
+            return FieldOption.DEFAULT
     }
 
 }
---
+++ test/integration/org/grails/msgpack/template/DomainModelFieldOptionReaderSpec.groovy
@@ -0,0 +1,121 @@
+package org.grails.msgpack.template
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import grails.plugin.spock.IntegrationSpec;
+
+import msgpack.Comment;
+import msgpack.User;
+
+import org.msgpack.template.BeansFieldEntry;
+import org.msgpack.template.FieldOption;
+import org.gmock.WithGMock;
+import spock.lang.Specification;
+
+@WithGMock
+class DomainModelFieldOptionReaderSpec extends IntegrationSpec {
+
+    DomainModelFieldOptionReader reader
+
+    def setup(){
+        reader = DomainModelFieldOptionReader.instance
+    }
+
+    def "the field which can be nullable in constraints is treated as Optional"(){
+
+        given:
+        def field = mock(BeansFieldEntry)
+        field.name.returns(fieldName).stub()
+        FieldOption result
+        play{
+            result = reader.read( msgpack.Message, field, FieldOption.DEFAULT )
+        }
+
+        expect:
+        result == option
+
+        where:
+        fieldName     | option
+        'note'        | FieldOption.OPTIONAL
+        'dateUpdated' | FieldOption.OPTIONAL
+        'postalId'    | FieldOption.OPTIONAL
+    }
+
+    def 'the field which can be blank in constraints is treated as Default'(){
+
+        given:
+        def field = mock(BeansFieldEntry)
+        field.name.returns('body').stub()
+
+        when:
+        FieldOption result
+        play{
+            result = reader.read( msgpack.Message, field, FieldOption.DEFAULT )
+        }
+
+        then:
+        result == FieldOption.DEFAULT
+    }
+
+    def 'the field which is not difnied in constraints is treated as Default'(){
+
+        given:
+        def field = mock(BeansFieldEntry)
+        field.name.returns(fieldName).stub()
+        FieldOption result
+        play{
+            result = reader.read( msgpack.Message, field, FieldOption.DEFAULT )
+        }
+
+        expect:
+        result == option
+
+        where:
+        fieldName     | option
+        'dateCreated' | FieldOption.DEFAULT
+        'owner'       | FieldOption.DEFAULT
+        'price'       | FieldOption.DEFAULT
+        'readCount'   | FieldOption.DEFAULT
+        'isPublic'    | FieldOption.DEFAULT
+    }
+
+    def 'a collection field is treated as Optional even if it has no constraints'(){
+
+        given:
+        def field = mock(BeansFieldEntry)
+        field.name.returns(fieldName).stub()
+        FieldOption result
+        play{
+            result = reader.read( msgpack.Message, field, FieldOption.DEFAULT )
+        }
+
+        expect:
+        result == option
+
+        where:
+        fieldName  | option
+        'props'    | FieldOption.OPTIONAL
+        'comments' | FieldOption.OPTIONAL
+    }
+
+    def 'id field and version field, which is added by grails, is treated as Optional'(){
+
+        given:
+        def field = mock(BeansFieldEntry)
+        field.name.returns(fieldName).stub()
+        FieldOption result
+        play{
+            result = reader.read( msgpack.Message, field, FieldOption.DEFAULT )
+        }
+
+        expect:
+        result == option
+
+        where:
+        fieldName | option
+        'id'      | FieldOption.OPTIONAL
+        'version' | FieldOption.OPTIONAL
+    }
+}
